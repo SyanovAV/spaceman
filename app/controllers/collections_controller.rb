a199e4352c8e97226af725c587fe5d47acd54e9e
Сьянов Александр Вячеславович <asyanov@phoenixit.ru>
2018-06-07 12:25:12 +0300

    add CRU

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,17 +1,40 @@
 class CollectionsController < ApplicationController
   def new
   end
+
+
+  def index
+    @collection = Collection.all
+  end
+
+  def show
+    @collection = Collection.find(params[:id])
+  end
+
   def create
-    @collection = Collection.new(params.require(:collection).permit(:name, :description))
+    @collection = Collection.new(collection_params)
   #  @requests = Request.new(params[:collection].permit(:raw_headers, :url, :method))
     @collection.save
   #  @requests.save
     redirect_to @collection
   end
-  def show
+
+  def edit
     @collection = Collection.find(params[:id])
   end
-  def index
-    @collection = Collection.all
+
+  def update
+    @collection = Collection.find(params[:id])
+
+    if @collection.update(collection_params)
+      redirect_to @collection
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def collection_params
+    params.require(:collection).permit(:name, :description)
   end
 end
