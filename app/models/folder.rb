076995626bb48ccc4813221a61d53e409707c011
andreev <vladislavandand@gmail.com>
2018-06-25 08:50:18 +0300

    WIP requests CRUD

--- app/models/folder.rb
+++
@@ -1,4 +0,0 @@
-class Folder < ApplicationRecord
-  belongs_to :items
-  has_many :requests
-end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   has_many :requests
-  belongs_to :collection, class_name: "Item", optional: true
   validates :name, presence: true,
             length: { minimum: 5 }
 end
